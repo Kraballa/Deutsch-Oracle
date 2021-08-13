f939a281d8e9f9a9803fb92cff63ff56b0aebe38
Vinzent Brömauer <vinz.corno@web.de>
2021-08-13 03:13:11 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace QSIM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Deutsch Oracle");
+            for (int i = 0; i < 4; i++)
+            {
+                QBit input = new QBit(0);
+                QBit output = new QBit(0);
+                input.X().H();
+                output.X().H();
+                QBit.Function(i, ref input, ref output);
+                input.H();
+                output.H();
+                if (input.Collapse() == 1 && output.Collapse() == 1)
+                {
+                    Console.WriteLine("the function was constant");
+                }
+                else if (input.Collapse() == 0 && output.Collapse() == 1)
+                {
+                    Console.WriteLine("the function was variable");
+                }
+                else
+                {
+                    Console.WriteLine("calculation error, retry");
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ QBit.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QSIM
+{
+    public class QBit
+    {
+        private static Random rand = new Random();
+
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public QBit(int val)
+        {
+            Set(val);
+        }
+
+        public QBit CNOT(QBit a)
+        {
+            return this;
+        }
+
+        public QBit H()
+        {
+            double val = 1 / Math.Sqrt(2);
+            double newA = A * val + B * val;
+            B = A * val - B * val;
+            A = newA;
+            return this;
+        }
+
+        public QBit X()
+        {
+            double val = B;
+            B = A;
+            A = val;
+            return this;
+        }
+
+        public QBit Set(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    A = 1; B = 0; break;
+                case 1:
+                    A = 0; B = 1; break;
+            }
+            return this;
+        }
+
+        public QBit Set(QBit b)
+        {
+            A = b.A;
+            B = b.B;
+            return this;
+        }
+
+        public double Measure()
+        {
+            return Math.Abs(0.5 - A * A / 2 + B * B / 2);
+        }
+
+        public int Collapse()
+        {
+            return rand.NextDouble() < Measure() ? 1 : 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}|{1}]", A, B);
+        }
+
+        public static void CNOT(ref QBit a, ref QBit b)
+        {
+            if (a.B == 0)
+            {
+                b.X();
+            }
+            else
+            {
+                a.B *= -1;
+            }
+        }
+
+        /// <summary>
+        /// Define the 4 possible functions on one bit, to operate them on a qbit,
+        /// as well as doubling input for reversability, and have them be iterable by i.
+        /// </summary>
+        public static void Function(int i, ref QBit input, ref QBit output)
+        {
+            switch (i)
+            {
+                case 0:
+                    Console.WriteLine("function {set 0}");
+                    break;
+                case 1:
+                    Console.WriteLine("function {set 1}");
+                    output.X();
+                    break;
+                case 2: //invert
+                    Console.WriteLine("function {invert}");
+                    CNOT(ref input, ref output);
+                    output.X();
+                    break;
+                case 3: //identity
+                    Console.WriteLine("function {identity}");
+                    CNOT(ref input, ref output);
+                    break;
+            }
+        }
+    }
+}
